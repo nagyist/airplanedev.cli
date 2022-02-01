05deec1b2ed022c4b75bfe6d797155753aad0c02
Colin <colin@airplane.dev>
2022-02-01 12:51:34 -0500

    Add EnvSlug to Deployer (#40)
    This is used by the CLI when deploying into an environment.
    
    I mistakenly removed it from this PR: https://github.com/airplanedev/lib/pull/38#discussion_r796807118

--- pkg/deploy/discover/discover.go
+++ pkg/deploy/discover/discover.go
@@ -45,6 +45,12 @@ type Discoverer struct {
 	TaskDiscoverers []TaskDiscoverer
 	Client          api.IAPIClient
 	Logger          logger.Logger
+
+	// EnvSlug is the slug of the environment to look for discovered tasks in.
+	//
+	// If a task is discovered, but doesn't exist in this environment, then the task
+	// is treated as missing.
+	EnvSlug string
 }
 
 // DiscoverTasks recursively discovers Airplane tasks.
@@ -87,7 +93,8 @@ func (d *Discoverer) DiscoverTasks(ctx context.Context, paths ...string) ([]Task
 				continue
 			}
 			task, err := d.Client.GetTask(ctx, api.GetTaskRequest{
-				Slug: slug,
+				Slug:    slug,
+				EnvSlug: d.EnvSlug,
 			})
 			if err != nil {
 				var missingErr *api.TaskMissingError
