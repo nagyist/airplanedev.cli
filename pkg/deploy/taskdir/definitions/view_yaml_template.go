a45ef8c7b1b71b24d2383c38f5e30efc58f4fb22
Rose Huang <rose@airplane.dev>
2022-07-18 16:13:17 -0400

    Remove env vars and update description for entrypoint in view yaml (#182)
    ## Description
    editing some content of the yaml
    
    ## Test plan
    rebuild CLI and init 
    
    <img width="652" alt="image" src="https://user-images.githubusercontent.com/34196250/179247823-4f78c7bd-af5d-4ca5-9546-44fb158c92aa.png">

--- pkg/deploy/taskdir/definitions/view_yaml_template.go
+++ pkg/deploy/taskdir/definitions/view_yaml_template.go
@@ -11,18 +11,7 @@ name: {{.name}}
 # A human-readable description for your view.
 # description: "My Airplane view"
 
-# The path to the directory containing the logic for this view. This
+# The path to the .view.tsx file containing the logic for this view. This
 # can be absolute or relative to the location of the definition file.
 entrypoint: {{.entrypoint}}
-
-# A map of environment variables to use when for the view. The value
-# should be an object; if specifying raw values, the value must be an object
-# with ` + "`value`" + ` mapped to the value of the environment variable; if
-# using config variables, the value must be an object with ` + "`config`" + `
-# mapped to the name of the config variable.
-# envVars:
-#   ENV_VAR_FROM_CONFIG:
-#     config: database_url
-#   ENV_VAR_FROM_VALUE:
-#     value: env_var_value
 `
