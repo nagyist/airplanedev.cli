d6b8d6b21065489bd615bdd4889a542b9fc9f1c2
Lee Weisberger <lee@airplane.dev>
2022-07-25 12:35:38 -0400

    Add error boundary to view when devving

--- cmd/airplane/views/dev/templates/main.tsx
+++ cmd/airplane/views/dev/templates/main.tsx
@@ -1,4 +1,10 @@
-import { Container, ThemeProvider, ViewProvider, setEnvVars } from "@airplane/views";
+import {
+  Container,
+  ThemeProvider,
+  ViewProvider,
+  setEnvVars,
+  ErrorBoundary,
+} from "@airplane/views";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./src/{{.Entrypoint}}";
@@ -7,15 +13,18 @@ setEnvVars(
   import.meta.env.AIRPLANE_API_HOST || "https://api.airplane.dev",
   import.meta.env.AIRPLANE_TOKEN,
   import.meta.env.AIRPLANE_API_KEY,
-  import.meta.env.AIRPLANE_ENV_SLUG,
+  import.meta.env.AIRPLANE_ENV_SLUG
 );
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
       <ViewProvider>
-        <Container size="xl" py={96}>
-          <App />
-        </Container>
+        <ErrorBoundary>
+          <Container size="xl" py={96}>
+            <App />
+          </Container>
+        </ErrorBoundary>
       </ViewProvider>
     </ThemeProvider>
   </React.StrictMode>
