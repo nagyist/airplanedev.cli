c286ff232d0106b7d89ea19017e11a55a1e3902e
Zhan Xiong Chin <zhan@airplane.dev>
2022-09-27 16:06:42 +0900

    Load envSlug from get param in main.tsx if it exists (#304)
    ## Description
    Otherwise if someone promotes a view between environments, we don't have a way of indicating to the view that the env is updated (since the env slug is baked into the view)
    
    ## Test plan
    Ran locally

--- pkg/build/views/main.tsx
+++ pkg/build/views/main.tsx
@@ -8,11 +8,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "{{.Entrypoint}}";
 
+const paramsEnvSlug = typeof window !== "undefined" ? (new URL(window.location)).searchParams.get("__env") : null;
+
 setEnvVars(
   import.meta.env.AIRPLANE_API_HOST || "https://api.airplane.dev",
   import.meta.env.AIRPLANE_TOKEN,
   import.meta.env.AIRPLANE_API_KEY,
-  import.meta.env.AIRPLANE_ENV_SLUG
+  paramsEnvSlug || import.meta.env.AIRPLANE_ENV_SLUG
 );
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
