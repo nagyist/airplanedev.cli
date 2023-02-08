8b763bf491115a9fd6bf2a6bc6e8e9d2776a1dd5
Justin Lu <justin@airplane.dev>
2023-02-08 21:44:58 +0000

    Report internal server errors to sentry (#950)
    ## Description
    Reports Studio 500s to sentry, similar to our prod API.
    
    ## Test plan
    Triggered a 500 manually and verified that the sentry reporting codepath was hit.

--- pkg/server/handlers/handlers.go
+++ pkg/server/handlers/handlers.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"unicode"
 
+	"github.com/airplanedev/cli/pkg/analytics"
 	"github.com/airplanedev/cli/pkg/server/state"
 	libhttp "github.com/airplanedev/lib/pkg/api/http"
 	"github.com/pkg/errors"
@@ -18,7 +19,7 @@ import (
 func Wrap(f func(ctx context.Context, w http.ResponseWriter, r *http.Request) error) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := f(r.Context(), w, r); err != nil {
-			WriteHTTPError(w, r, err)
+			WriteHTTPError(w, r, err, analytics.ReportError)
 			return
 		}
 	}
@@ -111,19 +112,21 @@ func Zip(state *state.State,
 	})
 }
 
-// WriteHTTPError writes an error to response and optionally logs it
-func WriteHTTPError(w http.ResponseWriter, r *http.Request, err error) {
+// WriteHTTPError writes an error to response and optionally reports it
+func WriteHTTPError(w http.ResponseWriter, r *http.Request, err error, report func(err error)) {
 	ctx := r.Context()
 
 	errStatusCode, retryable := GetErrorStatus(ctx, err)
 
-	// TODO: Log 500 errors to sentry
+	if errStatusCode.StatusCode >= http.StatusInternalServerError {
+		report(err)
+	}
 
 	out, err := json.Marshal(libhttp.ErrorResponse{
 		Error: errStatusCode.Msg,
 	})
 	if err != nil {
-		// TODO: Report to sentry
+		report(errors.Wrap(err, "marshaling error response"))
 		return
 	}
 	// This extra newline is for consistency with how we previously use an Encoder to write JSON responses.
@@ -135,7 +138,7 @@ func WriteHTTPError(w http.ResponseWriter, r *http.Request, err error) {
 	w.WriteHeader(errStatusCode.StatusCode)
 
 	if _, err := w.Write(out); err != nil {
-		// TODO: Report to sentry
+		report(errors.Wrap(err, "writing error response"))
 		return
 	}
 }
