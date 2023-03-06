d49341c509fe00b88a631c36d65e795f28420ab9
Rose Huang <rose@airplane.dev>
2023-03-07 05:46:33 +0800

    Add event listeners for errors and unhandledrejections to send messages to parent frame (#579)
    ## Description
    We add event listeners when we get an error event or unhandledrejection event in the view
    
    
    We duplicate some code here that we have in the views lib (ViewMessage and sendViewMessage) but this seems better than trying to export those out of views and having to deal with backward compatibility issues
    
    We also start sending the stack trace of the error so we can show that in the debug panel for errors that don't have a modal
    
    ## Test plan
    Testing alongside views and web changes

--- pkg/build/views/main.tsx
+++ pkg/build/views/main.tsx
@@ -8,6 +8,7 @@ import {
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "{{.Entrypoint}}";
+import hash from "object-hash";
 
 const isInStudio = Boolean({{.IsInStudio}});
 
@@ -33,7 +34,7 @@ const getQueryParam = (key: string) => {
 
 /**
  * ENV_VAR_BLOCK_LIST is the list of query params we do not want to set as environment variables.
- * since the user doesn't need to access them. 
+ * since the user doesn't need to access them.
 */
 const RUNTIME_ENV_VAR_BLOCK_LIST = ["AIRPLANE_SANDBOX_TOKEN", "AIRPLANE_VIEW_TOKEN"];
 
@@ -82,12 +83,65 @@ for (const [key, value] of getAllQueryParams()) {
     if (!title.startsWith("AIRPLANE_")) {
       title = `AIRPLANE_${title}`;
     }
-  } 
+  }
 
   if (!RUNTIME_ENV_VAR_BLOCK_LIST.includes(title)) {
     process.env[title] = val;
   }
 }
+
+// Intercept window error and unhandledrejection events and send them to the parent window.
+if (typeof window !== undefined) {
+  window.addEventListener("error", (event) => {
+    sendViewMessage({
+      type: "console",
+      messageType: "error",
+      message: "Uncaught Error: "+event.error.message,
+      stack: event.error.stack,
+      hash: hash(event.error),
+      time: Date.now(),
+    })
+  })
+
+  window.addEventListener("unhandledrejection", (event) => {
+    sendViewMessage({
+      type: "console",
+      messageType: "error",
+      message: "Unhandled Promise rejection: "+event.reason,
+      hash: hash(event.reason ?? "undefined"),
+      time: Date.now(),
+    })
+  })
+}
+
+export const sendViewMessage = (message: ViewMessage) => {
+  if (typeof window === undefined) {
+    return;
+  }
+  window.parent.postMessage(message, "*");
+};
+
+// Keep in sync with ErrorConsoleMessage from web/lib/views/ViewMessage.ts
+type ConsoleMessageBase = {
+  type: "console";
+  hash: string;
+  // unix timestamp in milliseconds
+  time: number;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  message?: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  optionalParams?: any[];
+};
+
+type ErrorConsoleMessage = {
+  messageType: "error";
+  id?: string;
+  component?: string;
+  stack?: string;
+} & ConsoleMessageBase;
+
+type ViewMessage = ErrorConsoleMessage
+
 
 const AppWrapper = () => {
   const { params } = useRouter();
