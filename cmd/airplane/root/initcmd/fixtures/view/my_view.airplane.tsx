a4c71666f68d5ee1b0d95931fad1cf91597a7b0d
Stephen Zhou <stephen@airplane.dev>
2023-03-30 10:22:38 -0700

    Adjust learn more blocks

--- cmd/airplane/root/initcmd/fixtures/view/my_view.airplane.tsx
+++ cmd/airplane/root/initcmd/fixtures/view/my_view.airplane.tsx
@@ -11,21 +11,21 @@ const MyView = () => {
       <Stack spacing={0}>
         <Heading level={3}>Learn more</Heading>
         <Stack direction="row">
-          <Callout variant="neutral" title="Overview" width="1/3">
-            {"Check out what you can build with views. "}
-            <Link href="https://docs.airplane.dev/views/overview" size="sm">
-              Read the docs.
-            </Link>
-          </Callout>
           <Callout variant="neutral" title="Build your first view" width="1/3">
             {"Walk through building a simple view in 15 minutes. "}
             <Link href="https://docs.airplane.dev/getting-started/views" size="sm">
               Read the docs.
             </Link>
           </Callout>
-          <Callout variant="neutral" title="Component library" width="1/3">
-            {"Browse the Views component library. "}
-            <Link href="https://docs.airplane.dev/views/components" size="sm">
+          <Callout variant="neutral" title="Templates" width="1/3">
+            {"Work off of one of our many ready-made templates. "}
+            <Link href="https://docs.airplane.dev/templates" size="sm">
+              See our templates.
+            </Link>
+          </Callout>
+          <Callout variant="neutral" title="Reference" width="1/3">
+            {"Learn more about how to supercharge your views. "}
+            <Link href="https://docs.airplane.dev" size="sm">
               Read the docs.
             </Link>
           </Callout>
