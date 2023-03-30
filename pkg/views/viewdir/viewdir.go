90130ad2cb124a98b864c837057f74b6f3ecc3c7
Colin King <colin@airplane.dev>
2023-03-30 15:47:02 -0700

    Fix a few minor lib references

--- pkg/views/viewdir/viewdir.go
+++ pkg/views/viewdir/viewdir.go
@@ -13,7 +13,7 @@ import (
 	"github.com/pkg/errors"
 )
 
-// TODO(zhan): this probably should be in airplanedev/lib.
+// TODO(zhan): this probably should be in airplanedev/cli.
 type ViewDirectoryInterface interface {
 	Root() string
 	EntrypointPath() string
