7e0b42ba142cfefb4009cfd35d84be1dea5451d9
Colin <colin@airplane.dev>
2023-05-02 12:12:08 -0400

    Add marshal method to view definition (#1110)
    ## Description
    
    This PR adds a `Marshal` method to `ViewDefinition` which aligns with what we have on the task `Definition` struct. This will be used in a follow-up PR where we add logic for updating View definitions.
    
    This PR also makes the `name` field optional, which matches the behavior of tasks. The JS SDK will automatically convert slugs into humanized strings and therefore already doesn't require a `name` field. I tested out what happens when you deploy a YAML task without a name and noticed that we don't handle it well (e.g. in the deploy logs or in the deploy UI it shows an empty string). Since this is an issue with tasks and only applies to YAML definitions, I'm punting on that for now (see https://linear.app/airplane/issue/CAP-2533/generate-names-for-tasksviews-without-names).
    
    ## Test plan
    
    Tests pass.

--- pkg/definitions/view_def.go
+++ pkg/definitions/view_def.go
@@ -13,13 +13,24 @@ import (
 	"github.com/xeipuuv/gojsonschema"
 )
 
+// ViewDefinition specifies a view's configuration.
+//
+// A definition is commonly serialized as JavaScript, co-located with the View's code. However, it can
+// also be serialized as YAML.
+//
+// Optional fields should have `omitempty` set. If omitempty doesn't work on the field's type, add an
+// `IsZero` method. See the `DefaultXYZDefinition` structs as an example. Add test cases to
+// TestDefinitionMarshal to confirm this behavior. This behavior is relied upon when updating
+// view definitions via `pkg/views.Update` (you should add test cases in the corresponding
+// `pkg/definitions/updaters` when adding new fields).
 type ViewDefinition struct {
-	Name         string               `json:"name"`
-	Slug         string               `json:"slug"`
-	Description  string               `json:"description,omitempty"`
-	Entrypoint   string               `json:"entrypoint"`
-	EnvVars      api.EnvVars          `json:"envVars,omitempty"`
-	DefnFilePath string               `json:"defnFilePath,omitempty"`
+	Slug        string      `json:"slug"`
+	Name        string      `json:"name,omitempty"`
+	Description string      `json:"description,omitempty"`
+	Entrypoint  string      `json:"entrypoint"`
+	EnvVars     api.EnvVars `json:"envVars,omitempty"`
+	// DefnFilePath is the absolute path to this View definition, if one exists.
+	DefnFilePath string               `json:"-"`
 	Base         buildtypes.BuildBase `json:"base,omitempty"`
 }
 
@@ -60,6 +71,39 @@ func (d *ViewDefinition) Unmarshal(format DefFormat, buf []byte) error {
 		return err
 	}
 	return nil
+}
+
+func (d ViewDefinition) Marshal(format DefFormat) ([]byte, error) {
+	switch format {
+	case DefFormatYAML:
+		// Use the JSON marshaler so we use MarshalJSON methods.
+		buf, err := yaml.MarshalWithOptions(d,
+			yaml.UseJSONMarshaler(),
+			yaml.UseLiteralStyleIfMultiline(true))
+		if err != nil {
+			return nil, err
+		}
+		return buf, nil
+
+	case DefFormatJSON:
+		// Use the YAML marshaler so we can take advantage of the yaml.IsZeroer check on omitempty.
+		// But make it use the JSON marshaler so we use MarshalJSON methods.
+		buf, err := yaml.MarshalWithOptions(d,
+			yaml.UseJSONMarshaler(),
+			yaml.JSON())
+		if err != nil {
+			return nil, err
+		}
+		// `yaml.Marshal` doesn't allow configuring JSON indentation, so do it after the fact.
+		var out bytes.Buffer
+		if err := json.Indent(&out, buf, "", "\t"); err != nil {
+			return nil, err
+		}
+		return out.Bytes(), nil
+
+	default:
+		return nil, errors.Errorf("unknown format: %s", format)
+	}
 }
 
 func (d *ViewDefinition) GenerateCommentedFile() ([]byte, error) {
---
+++ pkg/definitions/view_def_test.go
@@ -0,0 +1,134 @@
+package definitions
+
+import (
+	"testing"
+
+	"github.com/airplanedev/cli/pkg/api"
+	"github.com/airplanedev/cli/pkg/utils/pointers"
+	"github.com/stretchr/testify/require"
+)
+
+func TestViewDefinitionMarshal(t *testing.T) {
+	t.Parallel()
+	for _, test := range []struct {
+		name                    string
+		def                     ViewDefinition
+		overrideUnmarshalledDef *ViewDefinition
+		expectedYAML            string
+		expectedJSON            string
+	}{
+		{
+			name: "implicit defaults",
+			def: ViewDefinition{
+				Slug: "hello_world",
+			},
+			expectedYAML: `slug: hello_world
+entrypoint: ""
+`,
+			expectedJSON: `{
+	"slug": "hello_world",
+	"entrypoint": ""
+}
+`,
+		},
+		{
+			// This test sets all values explicitly to their defaults which should not be serialized.
+			name: "explicit defaults",
+			def: ViewDefinition{
+				Slug:        "hello_world",
+				Name:        "",
+				Description: "",
+				Entrypoint:  "",
+				EnvVars:     api.EnvVars{},
+			},
+			// When unmarshalled, the default values will not be set.
+			overrideUnmarshalledDef: &ViewDefinition{
+				Slug: "hello_world",
+			},
+			expectedYAML: `slug: hello_world
+entrypoint: ""
+`,
+			expectedJSON: `{
+	"slug": "hello_world",
+	"entrypoint": ""
+}
+`,
+		},
+		{
+			// This test sets all values to non-default values. All should be serialized.
+			name: "non-default values",
+			def: ViewDefinition{
+				Slug:        "hello_world",
+				Name:        "Hello World",
+				Description: "A starter view.",
+				Entrypoint:  "entrypoint.tsx",
+				EnvVars: api.EnvVars{
+					"TEST_ENV": api.EnvVarValue{
+						Value: pointers.String("hello world"),
+					},
+				},
+			},
+			expectedYAML: `slug: hello_world
+name: Hello World
+description: A starter view.
+entrypoint: entrypoint.tsx
+envVars:
+  TEST_ENV:
+    value: hello world
+`,
+			expectedJSON: `{
+	"slug": "hello_world",
+	"name": "Hello World",
+	"description": "A starter view.",
+	"entrypoint": "entrypoint.tsx",
+	"envVars": {
+		"TEST_ENV": {
+			"value": "hello world"
+		}
+	}
+}
+`,
+		},
+		{
+			name: "multiline strings",
+			def: ViewDefinition{
+				Slug:        "hello_world",
+				Description: "This View does magic:\n\n  - Step 1: look fly\n  - Step 2: fly\n  - Step 3: ????\n  - Step 4: PROFIT!!!\n",
+				Entrypoint:  "entrypoint.tsx",
+			},
+			// Use double quotes instead of backticks because this string contains two leading spaces that would get removed by a formatter.
+			expectedYAML: "slug: hello_world\ndescription: |\n  This View does magic:\n  \n    - Step 1: look fly\n    - Step 2: fly\n    - Step 3: ????\n    - Step 4: PROFIT!!!\nentrypoint: entrypoint.tsx\n",
+			expectedJSON: `{
+	"slug": "hello_world",
+	"description": "This View does magic:\n\n  - Step 1: look fly\n  - Step 2: fly\n  - Step 3: ????\n  - Step 4: PROFIT!!!\n",
+	"entrypoint": "entrypoint.tsx"
+}
+`,
+		},
+	} {
+		test := test // rebind for parallel tests
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			require := require.New(t)
+
+			for _, fmt := range []DefFormat{DefFormatYAML, DefFormatJSON} {
+				expected := test.expectedYAML
+				if fmt == DefFormatJSON {
+					expected = test.expectedJSON
+				}
+
+				bytestr, err := test.def.Marshal(fmt)
+				require.NoError(err)
+				require.Equal(expected, string(bytestr))
+				d := ViewDefinition{}
+				err = d.Unmarshal(fmt, []byte(expected))
+				require.NoError(err)
+				if test.overrideUnmarshalledDef != nil {
+					require.Equal(*test.overrideUnmarshalledDef, d)
+				} else {
+					require.Equal(test.def, d)
+				}
+			}
+		})
+	}
+}
