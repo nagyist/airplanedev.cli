38b4817d64820e195ef94a2b23733798603fd794
Colin <colin@airplane.dev>
2023-05-02 10:46:39 -0400

    Fix race condition in debounce tests (#1109)
    ## Description
    
    I couldn't reproduce the race condition that happened ([gha](https://github.com/airplanedev/cli/actions/runs/4843002501/jobs/8630307588)) but it appears to be due to reads on `called`, so I switched to an atomic integer. This should prevent similar race conditions.
    
    ## Test plan
    
    Tests pass locally. Tests aren't flaky on this PR's CI run. If anyone sees these issues again, I can TAL again.
    
    ```
    cli ▶ go test -race ./pkg/utils/... -run TestDebounce -count=1000
    ?   	github.com/airplanedev/cli/pkg/utils/airplane_directory	[no test files]
    ?   	github.com/airplanedev/cli/pkg/utils/cryptox	[no test files]
    ?   	github.com/airplanedev/cli/pkg/utils/github	[no test files]
    ?   	github.com/airplanedev/cli/pkg/utils/handlebars	[no test files]
    ?   	github.com/airplanedev/cli/pkg/utils/logger	[no test files]
    ?   	github.com/airplanedev/cli/pkg/utils/pathcase	[no test files]
    ?   	github.com/airplanedev/cli/pkg/utils/pointers	[no test files]
    ?   	github.com/airplanedev/cli/pkg/utils/tracing	[no test files]
    ok  	github.com/airplanedev/cli/pkg/utils	37.942s
    ok  	github.com/airplanedev/cli/pkg/utils/bufiox	0.319s [no tests to run]
    ok  	github.com/airplanedev/cli/pkg/utils/fsx	0.258s [no tests to run]
    ```

--- pkg/utils/debounce_test.go
+++ pkg/utils/debounce_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -11,120 +12,120 @@ import (
 func TestDebounce(t *testing.T) {
 	require := require.New(t)
 
-	called := 0
+	called := atomic.Int32{}
 	clock := clock.NewMock()
 	f := DebounceWithOpts(DebounceOpts{
 		Delay: time.Second,
 		clock: clock,
 	}, func() {
-		called++
+		called.Add(1)
 	})
 
 	// The function has not been called yet.
-	require.Equal(0, called)
+	require.EqualValues(0, called.Load())
 
 	f()
 	f()
 	f()
 
 	// The function has still not been called because delay hasn't elapsed.
-	require.Equal(called, 0)
+	require.EqualValues(0, called.Load())
 
 	// Advance 1s. This will cause the function to get invoked.
 	clock.Add(time.Second)
-	require.Equal(called, 1)
+	require.EqualValues(1, called.Load())
 }
 
 func TestDebounceLeading(t *testing.T) {
 	require := require.New(t)
 
-	called := 0
+	called := atomic.Int32{}
 	clock := clock.NewMock()
 	f := DebounceWithOpts(DebounceOpts{
 		Delay:   time.Second,
 		Leading: true,
 		clock:   clock,
 	}, func() {
-		called++
+		called.Add(1)
 	})
 
 	// The function should be called immediately.
 	f()
 	f()
 	f()
-	require.Equal(1, called)
+	require.EqualValues(1, called.Load())
 
 	// The function should not be called again if <delay.
 	clock.Add(time.Second / 2)
-	require.Equal(1, called)
+	require.EqualValues(1, called.Load())
 
 	// Once delay time has passed, it should be called like normal.
 	clock.Add(time.Second / 2)
-	require.Equal(2, called)
+	require.EqualValues(2, called.Load())
 
 	// If called only once, it should only be called on the leading edge.
 	clock.Add(time.Second)
 	f()
-	require.Equal(3, called)
+	require.EqualValues(3, called.Load())
 	clock.Add(time.Second)
-	require.Equal(3, called)
+	require.EqualValues(3, called.Load())
 
 	// If called more than once, it should be called on both the leading and trailing edges.
 	clock.Add(time.Second)
 	f()
-	require.Equal(4, called)
+	require.EqualValues(4, called.Load())
 	f()
 	f()
-	require.Equal(4, called)
+	require.EqualValues(4, called.Load())
 	clock.Add(time.Second)
-	require.Equal(5, called)
+	require.EqualValues(5, called.Load())
 }
 
 func TestDebounceMaxWait(t *testing.T) {
 	require := require.New(t)
 
-	called := 0
+	called := atomic.Int32{}
 	clock := clock.NewMock()
 	f := DebounceWithOpts(DebounceOpts{
 		Delay:   2 * time.Second,
 		MaxWait: 4 * time.Second,
 		clock:   clock,
 	}, func() {
-		called++
+		called.Add(1)
 	})
 
 	// Keep invoking the function before the delay can elapse. The underlying function will not
 	// get called until we hit maxWait.
 	f()
-	require.Equal(0, called)
+	require.EqualValues(0, called.Load())
 	clock.Add(time.Second) // 1s
 	f()
-	require.Equal(0, called)
+	require.EqualValues(0, called.Load())
 	clock.Add(time.Second) // 2s
 	f()
-	require.Equal(0, called)
+	require.EqualValues(0, called.Load())
 	clock.Add(time.Second) // 3s
 	f()
-	require.Equal(0, called)
+	require.EqualValues(0, called.Load())
 	clock.Add(time.Second) // 4s
 	// The function should get called automatically due to maxWait.
-	require.Equal(1, called)
+	require.EqualValues(1, called.Load())
 
 	// The function should not get called again (e.g. for the trailing edge).
 	clock.Add(10 * time.Second)
-	require.Equal(1, called)
+	require.EqualValues(1, called.Load())
 
 	// But if we do another function call, it should get called again.
 	f()
-	require.Equal(1, called)
+	require.EqualValues(1, called.Load())
 	clock.Add(2 * time.Second)
-	require.Equal(2, called)
+	require.EqualValues(2, called.Load())
 }
 
 func TestDebounceMaxWaitLeading(t *testing.T) {
 	require := require.New(t)
 
-	called := 0
+	called := atomic.Int32{}
 	clock := clock.NewMock()
 	f := DebounceWithOpts(DebounceOpts{
 		Delay:   2 * time.Second,
@@ -132,51 +133,51 @@ func TestDebounceMaxWaitLeading(t *testing.T) {
 		MaxWait: 4 * time.Second,
 		clock:   clock,
 	}, func() {
-		called++
+		called.Add(1)
 	})
 
 	// Keep invoking the function before the delay can elapse. The underlying function will get
 	// called immediately because leading is true, but otherwise won't get called until we hit maxWait.
 	f()
-	require.Equal(1, called)
+	require.EqualValues(1, called.Load())
 	clock.Add(time.Second) // 1s
 	f()
-	require.Equal(1, called)
+	require.EqualValues(1, called.Load())
 	clock.Add(time.Second) // 2s
 	f()
-	require.Equal(1, called)
+	require.EqualValues(1, called.Load())
 	clock.Add(time.Second) // 3s
 	f()
-	require.Equal(1, called)
+	require.EqualValues(1, called.Load())
 	clock.Add(time.Second) // 4s
 	// The function should get called automatically due to maxWait.
-	require.Equal(2, called)
+	require.EqualValues(2, called.Load())
 
 	// If we do another function call, it's been <delay since the function was invoked (due to maxWait),
 	// so it should not get called immediately.
 	f()
-	require.Equal(2, called)
+	require.EqualValues(2, called.Load())
 	clock.Add(10 * time.Second)
 	// But it should eventually fire:
-	require.Equal(3, called)
+	require.EqualValues(3, called.Load())
 }
 
 func TestDebounceMaxWaitEqualDelay(t *testing.T) {
 	require := require.New(t)
 
-	called := 0
+	called := atomic.Int32{}
 	clock := clock.NewMock()
 	f := DebounceWithOpts(DebounceOpts{
 		Delay:   time.Millisecond,
 		MaxWait: time.Millisecond,
 		clock:   clock,
 	}, func() {
-		called++
+		called.Add(1)
 	})
 
 	// Should only be called once, even though delay and maxWait elapse at the same time.
 	f()
-	require.Equal(0, called)
+	require.EqualValues(0, called.Load())
 	clock.Add(time.Millisecond)
-	require.Equal(1, called)
+	require.EqualValues(1, called.Load())
 }
